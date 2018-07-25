ae4a1fe8bf393a5e0db7a28377e96ed739bbcd84
wkwksama <mailpakgasing@gmail.com>
2018-07-26 01:26:54 +0700

    perbaiki skema user

--- server/models/user.js
+++ server/models/user.js
@@ -3,11 +3,14 @@ let mongoose = require("mongoose");
 let userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    emailVerified: { type: Boolean, default: false},
     salt: String,
-    isAdmin: { type: Boolean, default: false}, // is this user a mimin ?
-    isActive: { type: Boolean, default: false}, // is this user active in using this system ?
-    isBanned: { type: Boolean, default: false}, // is this user has been banned ?
+    password: { type: String, required: true },
+    roles: Array,
+    banned: {
+      status: { type: Boolean, default: false}, // is this user has been banned ?
+      reason: String
+    },
     info: {
       fullName: String,
       nickName: String,
@@ -17,9 +20,3 @@ let userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
-
-// --------- Explanation ---------
-
-// isActive adalah user itu aktif atau tidak. ini digunakan untuk sistem membantu ferivikasi. untuk mengaktifkan akun user akan diberikan suatu cara tertentu untuk mengaktifkan akun miliknya. contoh: diberi salt untuk membuat password
-
-// isBanned adalah kondisi user itu melakukan pelanggaran. banned dilakukan oleh admin. user yang dibanned akan memiliki status banned yang berarti user itu sengaja dibanned olah admin
