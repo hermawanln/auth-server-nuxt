ec7aee08d051b5ba365603a139542a79114cee40
wkwksama <mailpakgasing@gmail.com>
2018-07-26 18:40:22 +0700

    vee-validate

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -2,7 +2,15 @@
   <v-layout justify-center>
     <v-card>
       <v-card-title>Login</v-card-title>
-      <v-card-text>ayo login jon</v-card-text>
+      <v-card-text>
+        <!-- TODO: implemen vee validate -->
+        <v-text-field
+          name="email"
+          label="Email"
+          id="email"
+          v-model="email"
+          required></v-text-field>
+      </v-card-text>
     </v-card>
   </v-layout>
 </template>
