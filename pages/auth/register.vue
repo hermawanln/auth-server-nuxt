096a1d15588adf54b9168a113a456667f4d02656
wkwksama <mailpakgasing@gmail.com>
2018-07-27 00:26:42 +0700

    ok register user

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -17,6 +17,7 @@
             :error-messages="errors.collect('fullName')"
             v-validate="'required'"
             data-vv-name="fullName"
+            data-vv-as="Nama Lengkap"
             required
           ></v-text-field>
           <v-text-field
@@ -28,12 +29,14 @@
             required
           ></v-text-field>
           <v-text-field
+            ref="password"
             v-model="password"
             label="Password"
             :counter="5"
             :error-messages="errors.collect('password')"
             v-validate="'required|min:6'"
             data-vv-name="password"
+            data-vv-as="Password"
             required
             hint="setidaknya 6 karakter"
             :append-icon="passwordVisibility ? 'visibility' : 'visibility_off'"
@@ -42,11 +45,12 @@
           ></v-text-field>
           <v-text-field
             v-model="confirmPassword"
-            label="Confirm Password"
+            label="Konfirmasi Password"
             :counter="5"
             :error-messages="errors.collect('confirmPassword')"
-            v-validate="'required|min:6'"
+            v-validate="'required|min:6|confirmed:password'"
             data-vv-name="confirmPassword"
+            data-vv-as="Konfirmasi Password"
             required
             hint="setidaknya 6 karakter"
             :append-icon="confirmPasswordVisibility ? 'visibility' : 'visibility_off'"
@@ -57,7 +61,7 @@
         <v-card-actions>
           <v-btn color="primary" flat>Login saja</v-btn>
           <v-spacer></v-spacer>
-          <v-btn color="primary">Buat Akun</v-btn>
+          <v-btn @click="submit()" :loading="loadingSubmit" color="primary">Buat Akun</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -68,6 +72,7 @@
 export default {
   data () {
     return {
+      loadingSubmit: false,
       email: '',
       fullName: '',
       username: '',
@@ -75,6 +80,35 @@ export default {
       confirmPassword: '',
       passwordVisibility: true,
       confirmPasswordVisibility: true
+    }
+  },
+  methods: {
+    submit () {
+      this.$validator.validateAll().then(valid => {
+        if (valid) {
+          this.loadingSubmit = true
+          this.$axios.$post('/api/auth/register', {
+            fullName: this.fullName,
+            email: this.email,
+            username: this.username,
+            password: this.confirmPassword
+          }).then(response => {
+            console.log('ok', response);
+            this.clear()
+            this.loadingSubmit = false
+          }).catch(err => {
+            console.log('err', err);
+            this.loadingSubmit = false
+          })
+        }
+      })
+    },
+    clear () {
+      this.email = ''
+      this.username = ''
+      this.fullName = ''
+      this.password = ''
+      this.confirmPassword = ''
     }
   }
 }
